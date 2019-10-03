a3f2cc8b93d3a7adf29163d701033930cf2af8f0
Andres Orozco <madrid_koz@hotmail.com>
2019-10-03 12:47:22 -0500

    Buena

--- app/src/main/java/com/example/calculadorapractica/MainActivity.java
+++ app/src/main/java/com/example/calculadorapractica/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.calculadorapractica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public EditText pantalla;
     public double operando1, operando2, res;
     int operacion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pantalla = findViewById(R.id.caja);
     }
     public void btn1(View view) {
         String cap = pantalla.getText().toString();
@@ -23,113 +26,114 @@ public class MainActivity extends AppCompatActivity{
 
     public void btn2(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "2";
         pantalla.setText(cap);
     }
 
-    public void btn3(View v) {
+    public void btn3(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "3";
         pantalla.setText(cap);
     }
 
-    public void btn4(View v) {
+    public void btn4(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "4";
         pantalla.setText(cap);
     }
 
-    public void btn5(View v) {
+    public void btn5(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "5";
         pantalla.setText(cap);
     }
 
-    public void btn6(View v) {
+    public void btn6(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "6";
         pantalla.setText(cap);
     }
 
-    public void btn7(View v) {
+    public void btn7(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "7";
         pantalla.setText(cap);
     }
 
-    public void btn8(View v) {
+    public void btn8(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "8";
         pantalla.setText(cap);
     }
 
-    public void btn9(View v) {
+    public void btn9(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "9";
         pantalla.setText(cap);
     }
 
-    public void btn0(View v) {
+    public void btn0(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + "0";
         pantalla.setText(cap);
     }
-    public void btnpunto(View v) {
+    public void btnpunto(View view) {
         String cap = pantalla.getText().toString();
-        cap = cap + "1";
+        cap = cap + ".";
         pantalla.setText(cap);
     }
 
-    public void suma(View v) {
+    public void suma(View view) {
         try {
             String aux1 = pantalla.getText().toString();
             operando1 = Double.parseDouble(aux1);
         } catch (NumberFormatException nfe) {
-            pantalla.setText("");
+            pantalla.setText("+");
             operacion = 1;
         }
     }
 
-    public void resta(View v) {
+    public void resta(View view) {
         try {
             String aux1 = pantalla.getText().toString();
             operando1 = Double.parseDouble(aux1);
         } catch (NumberFormatException nfe) {
-            pantalla.setText("");
+            pantalla.setText("-");
             operacion = 2;
         }
     }
 
-    public void multiplicacion(View v) {
+    public void multiplicacion(View view) {
         try {
             String aux1 = pantalla.getText().toString();
             operando1 = Double.parseDouble(aux1);
         } catch (NumberFormatException nfe) {
-            pantalla.setText("");
+            pantalla.setText("x");
             operacion = 3;
         }
     }
 
-    public void division(View v) {
+    public void division(View view) {
         try {
             String aux1 = pantalla.getText().toString();
             operando1 = Double.parseDouble(aux1);
         } catch (NumberFormatException nfe) {
-            pantalla.setText("");
+            pantalla.setText("/");
             operacion = 4;
         }
     }
 
-    public void porcentaje(View v) {
+    public void porcentaje(View view) {
         try {
             String aux1 = pantalla.getText().toString();
             operando1 = Double.parseDouble(aux1);
         } catch (NumberFormatException nfe) {
-            pantalla.setText("");
+            pantalla.setText("%");
             operacion = 5;
         }
     }
-    public void igual(View v){
+    @SuppressLint("SetTextI18n")
+    public void igual(View view){
         try {
             String aux2=pantalla.getText().toString();
             operando2=Double.parseDouble(aux2);
@@ -159,10 +163,15 @@ public class MainActivity extends AppCompatActivity{
         pantalla.setText(""+res);
         operando1=res;
     }
-    public void clear(View v){
+    public void clear(View view){
         pantalla.setText("");
         operando1 = 0.0;
         operando2 = 0.0;
         res= 0.0;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
